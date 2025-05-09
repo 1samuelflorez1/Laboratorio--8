1bd1dd0a069f28c386171609569a9bbf9f88ba3f
samuel florez <samuel.f.v.t.p@gmail.com>
2025-05-09 16:51:50 -0500

    se realizo la segunda patrte de mostrar tareas

--- app.js
+++ app.js
@@ -14,12 +14,41 @@ function obtenerTareasLocalStorage() {
 }
 
 // Guardar tareas en localStorage
-
+function guardarTareasLocalStorage(tareas) {
+  localStorage.setItem('tareas', JSON.stringify(tareas))
 }
-
 // Mostrar tareas en el DOM
 function mostrarTareas() {
-  
+  const tareas = obtenerTareasLocalStorage()
+  listaTareas.innerHTML = ''
+
+  tareas.forEach((texto, index) => {
+    const tareaDiv = document.createElement('div')
+    tareaDiv.className = 'tarea'
+
+    const textoTarea = document.createElement('p')
+    textoTarea.className = 'texto-tarea'
+    textoTarea.textContent = texto
+
+    const botonesDiv = document.createElement('div')
+    botonesDiv.className = 'botones-tarea'
+
+    const btnOk = document.createElement('button')
+    btnOk.className = 'btn_ok'
+    btnOk.textContent = '✔︎'
+    btnOk.onclick = () => completarTarea(index)
+
+    const btnEliminar = document.createElement('button')
+    btnEliminar.className = 'btn_eliminar'
+    btnEliminar.textContent = '✘'
+    btnEliminar.onclick = () => eliminarTarea(index)
+
+    botonesDiv.appendChild(btnOk)
+    botonesDiv.appendChild(btnEliminar)
+    tareaDiv.appendChild(textoTarea)
+    tareaDiv.appendChild(botonesDiv)
+    listaTareas.appendChild(tareaDiv)
+  })
 }
 
 // Marcar tarea como completada visualmente
